f6de103831f92f20fdc764d519a7ff6c04dc524c
jakfils <jakfilstechno@gmail.com>
2023-06-26 20:05:25 +0100

    Add expire sub-title expires day dynamically

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ const timerDaysValue = document.getElementById("timer-days");
 const timerHoursValue = document.getElementById("timer-hours");
 const timerMinsValue = document.getElementById("timer-mins");
 const timerSecsValue = document.getElementById("timer-secs");
+const dateElement = document.getElementById("date");
 
 // Funtion convert seconds to Date, Hour, Time, Mins and Secs
-function secondsToDhms(seconds) {
+const secondsToDhms = (seconds) => {
   seconds = Number(seconds);
   let d = Math.floor(seconds / (3600 * 24));
   let h = Math.floor((seconds % (3600 * 24)) / 3600);
@@ -17,10 +18,35 @@ function secondsToDhms(seconds) {
   let mDisplay = m >= 10 ? m : `0${m}`;
   let sDisplay = s >= 10 ? s : `0${s}`;
   return [dDisplay, hDisplay, mDisplay, sDisplay];
-}
+};
+// Function Add number of days to current date
+const addDays = (date, days) => {
+  const newDate = new Date(date.getTime());
+  console.log(newDate);
+  newDate.setDate(newDate.getDate() + days);
+  console.log(newDate);
+  return newDate;
+};
+//get Current date
+const today = new Date();
+//Set Expire Date
+const expireDate = addDays(today, 10);
+// Expire Date toLocalString
+const dateToShow = expireDate.toLocaleString("en-US", {
+  day: "numeric",
+  month: "long",
+  weekday: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
 
-// Initial number of seconds
-let seconds = 894999;
+// Show expire date
+dateElement.textContent = dateToShow;
+
+// Initial number of seconds (10days)
+let seconds = 863999;
 
 // Update seconds and show timer
 setInterval(() => {
